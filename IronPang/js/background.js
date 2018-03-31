f8e5fa22e2c3b4aff601d824c6ca9f21b1294749
Alejandro <alexibape@hotmail.com>
2018-03-31 23:28:58 +0200

    One background image

--- IronPang/js/background.js
+++ IronPang/js/background.js
@@ -2,7 +2,7 @@ function Background(game) {
   this.game = game;
 
   this.img = new Image();
-  this.img.src ="";
+  this.img.src ="img/1.png";
 
   this.x = 0;
   this.y = 0;
@@ -11,5 +11,11 @@ function Background(game) {
 
 
 Background.prototype.draw = function() {
-
+  this.game.ctx.drawImage(
+    this.img,
+    this.x,
+    this.y,
+    this.game.canvas.width,
+    this.game.canvas.height
+  )
 };
--- IronPang/js/game.js
+++ IronPang/js/game.js
@@ -2,7 +2,7 @@ function Game (canvasId) {
   this.canvas = document.getElementById(canvasId);
   this.ctx = this.canvas.getContext("2d");
   this.background = new Background(this);
-  this.player = new Player(this);
+  //this.player = new Player(this);
 
 
 
@@ -25,16 +25,16 @@ Game.prototype.start = function() {
 };
 
 Game.prototype.clear = function() {
-  this.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 };
 
 
 Game.prototype.draw = function() {
   this.background.draw();
-  this.player.draw();
+  //this.player.draw();
 
 };
 
 Game.prototype.moveAll = function() {
-  this.player.move();
+  //this.player.move();
 };
