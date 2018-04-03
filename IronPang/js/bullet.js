058e6cb50be2ce6a3caca3e06409ebbafa388dc8
Alejandro <alexibape@hotmail.com>
2018-04-03 15:52:37 +0200

    Collision between bullets and ball

--- IronPang/js/bullet.js
+++ IronPang/js/bullet.js
@@ -2,16 +2,26 @@ function Bullet(game) {
   this.game = game;
 
   this.x = this.game.player.x;
-  this.y = this.game.player.y;
-  this.vy = 10;
+  this.y = 350;
+  
+  this.vy = 20;
+  this.img = new Image();
+  this.img.src = "img/mine-bullet.png"; 
+  this.w = 50;
+  this.h = 20;
 
 }
 
 
 Bullet.prototype.draw = function() {
   
-  this.game.ctx.fillRect(this.x, this.y, 10, 10);
-  this.game.ctx.strokeStyle = "blue";
+  this.game.ctx.drawImage(
+    this.img,
+    this.x,
+    this.y,
+    this.w,
+    this.y
+  )
 };
 
 
--- IronPang/js/game.js
+++ IronPang/js/game.js
@@ -2,7 +2,7 @@ function Game(canvasId) {
   this.canvas = document.getElementById(canvasId);
   this.ctx = this.canvas.getContext("2d");
 
-  //this.audio = new Audio (this);
+  this.audio = new Audio ("sounds/01.mp3");
   this.player = new Player(this);
   this.ball = new Ball(this);
 
@@ -10,12 +10,14 @@ function Game(canvasId) {
 }
 
 Game.prototype.start = function() {
+  this.audio.play();
   this.intervalId = setInterval(
     function() {
       this.clear();
       this.draw();
       this.moveAll();
       this.checkCollision(this.player, this.ball);
+      this.checkBulletCollision(this.player, this.ball)
       this.clearBullets();
     }.bind(this),
     16
@@ -44,12 +46,23 @@ Game.prototype.moveAll = function() {
 Game.prototype.checkCollision = function (player,obstacle) {
   if (player.x < obstacle.x + obstacle.r && player.x + player.w > obstacle.x &&
     player.y < obstacle.y + obstacle.r && player.y + player.h > obstacle.y) {
-    console.log("CHOCASTE")
+    console.log("COLLISION")
  }
 }
 
+Game.prototype.checkBulletCollision = function (player, ball) {
+for (var i = 0; i < this.player.bullets.length; i++) {
+  console.log(this.player.bullets[i])
+    if (this.player.bullets[i].x < ball.x + ball.r && this.player.bullets[i].x + this.player.bullets[i].w > ball.x &&
+      this.player.bullets[i].y < ball.y + ball.r && this.player.bullets[i].y + this.player.bullets[i].h > ball.y){
+        console.log("Bullet Collision")
+      }
+    } 
+}
+
+
 Game.prototype.clearBullets = function() {
-  debugger
+  
   this.player.bullets = this.player.bullets.filter(
     function(e) {
       return e.y > 0;
